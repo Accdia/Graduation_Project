c6e70a33340e647ba86ae0e9a10e94f68722a4be
Accdia <accidia12139@outlook.com>
2023-03-05 22:34:37 +0800

    更改位置

--- Python/function/transfer.py
+++ Python/function/transfer.py
@@ -14,9 +14,10 @@ def transfer_image(image_file_path, destination_address):
     network_usage = psutil.net_io_counters().bytes_sent
     # network_usage = psutil.net_io_counters().bytes_sent / elapsed_time
     print(f"Network usage: {network_usage} bytes/second")
-    print(f"elapsed_time: {elapsed_time} bytes/second")
+    # print(f"elapsed_time: {elapsed_time} bytes/second")
 
 
 if __name__ == '__main__':
     # example usage
-    transfer_image('image.jpg', 'https://example.com/upload')
+    transfer_image('image/image.jpg', 'https://example.com/upload')
+    transfer_image('image/00.jpg', 'https://example.com/upload')
