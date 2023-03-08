3beeda5a600ec980ce897f56bd00728edafcc501
Accdia <accidia12139@outlook.com>
2023-03-08 18:05:30 +0800

    完成初步高斯设计

--- Python/function/GaussianBlur.py
+++ Python/function/GaussianBlur.py
@@ -1,18 +1,21 @@
 import cv2
 import matplotlib.pyplot as plt
-
+from PIL import Image
 
 if __name__ == '__main__':
     # 读取图像
     img = cv2.imread('../image/image.jpg')
 
     # 高斯滤波
-    blur = cv2.GaussianBlur(img,(5,5),0,borderType=cv2.BORDER_REPLICATE)
-    # cv2.imwrite('blur.jpg', blur)
+    blur = cv2.GaussianBlur(img, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)
+    cv2.imwrite('../image/blur.jpg', blur)
+    blur1 = Image.open('../image/blur.jpg')
+    blur2 = blur1.convert('L')
+    blur2.save('../image/blur.jpg')
 
     # 显示结果
-    plt.subplot(121),plt.imshow(img),plt.title('Original')
+    plt.subplot(121), plt.imshow(img), plt.title('Original')
     plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(blur),plt.title('GaussianBlur')
+    plt.subplot(122), plt.imshow(blur), plt.title('GaussianBlur')
     plt.xticks([]), plt.yticks([])
     plt.show()
