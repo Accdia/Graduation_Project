2db45425afefbe6fb7b33c0dea9e65686ef71ecc
Accdia <accidia12139@outlook.com>
2023-03-08 18:34:15 +0800

    添加获取大小的程序

--- Python/function/Image_information.py
+++ Python/function/Image_information.py
@@ -11,3 +11,28 @@ def image_color_depth(image):
 def image_size(image):
     size = list(image.size)
     return size[0] * size[1]
+
+
+# 获取文件大小（可直接嵌入工程使用）
+# input:文件路径
+# output：文件大小，单位
+import os
+
+
+def GetFileSize(filepath):
+    size = os.path.getsize(filepath)  # 返回的是字节大小
+    '''
+    为了更好地显示，应该时刻保持显示一定整数形式，即单位自适应
+    '''
+    if size < 1024:
+        return '%i' % size + 'size'
+    else:
+        KBX = size / 1024
+        if KBX < 1024:
+            return '%.1f' % float(KBX) + 'KB'
+        else:
+            MBX = KBX / 1024
+            if MBX < 1024:
+                return '%.1f' % float(MBX) + 'MB'
+            else:
+                return '%.1f' % float(MBX/1024) + 'GB'
