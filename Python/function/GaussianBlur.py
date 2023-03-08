c69d1ec02e081f7a23f6475b0a36eef0fe162f96
Accdia <accidia12139@outlook.com>
2023-03-08 22:33:31 +0800

    优化内容

--- Python/function/GaussianBlur.py
+++ Python/function/GaussianBlur.py
@@ -4,28 +4,34 @@ import numpy
 from function import Image_information
 
 
-def gaussian(image):
+def gaussian(size, image):
     # image = cv2.imread(path)  # 读取图像
-    blur = cv2.GaussianBlur(image, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)  # 高斯滤波
-    blur = Image.fromarray(numpy.uint8(blur))  # 将数组转为图像
-    blur = blur.convert('L')  # 将图像位深度转为8位
-    blur.save('image/GaussianBlur.jpg')  # 保存图像
-    print('Original image size: ', Image_information.GetFileSize('image/image.jpg'))
-    print('GaussianBlur image size: ', Image_information.GetFileSize('image/GaussianBlur.jpg'))  # 获取文件大小
+    # 高斯滤波
+    blur = cv2.GaussianBlur(image, (size, size), 0, borderType=cv2.BORDER_REPLICATE)
+    # 将数组转为图像
+    blur = Image.fromarray(numpy.uint8(blur))
+    # 将图像位深度转为8位
+    blur = blur.convert('L')
+    # 保存图像
+    blur.save('image' + '/GaussianBlur_' + str(size) + '.jpg')
+    # blur.save('image/GaussianBlur.jpg')
+    # 输出文件大小
+    print('GaussianBlur size =', size, 'image size =',
+          Image_information.GetFileSize('image' + '/GaussianBlur_' + str(size) + '.jpg'))
+    # print('Original image size: ', Image_information.GetFileSize('image/image.jpg'))
+    # print('GaussianBlur image size: ', Image_information.GetFileSize('image/GaussianBlur.jpg'))  # 获取文件大小
 
     return blur
 
 # if __name__ == '__main__':
 #     image = cv2.imread('../image/image.jpg')  # 读取图像
-#     blur = cv2.GaussianBlur(image, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)  # 高斯滤波
+#     blur = cv2.GaussianBlur(image, (7, 7), 0, borderType=cv2.BORDER_REPLICATE)  # 高斯滤波
 #     blur = Image.fromarray(numpy.uint8(blur))  # 将数组转为图像
 #     blur = blur.convert('L')  # 将图像位深度转为8位
 #     blur.save('../image/GaussianBlur.jpg')  # 保存图像
 #     print('Original image size: ', Image_information.GetFileSize('../image/image.jpg'))
 #     print('GaussianBlur image size: ', Image_information.GetFileSize('../image/GaussianBlur.jpg'))  # 获取文件大小
-#
 
-    #
     # # 读取图像
     # img = cv2.imread('../image/image.jpg')
     #
