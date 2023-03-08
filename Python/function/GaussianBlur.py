a1c8ff4a9a42b80dfa7ded725703a6f45a57f6d5
Accdia <accidia12139@outlook.com>
2023-03-08 21:48:55 +0800

    修改为函数形式

--- Python/function/GaussianBlur.py
+++ Python/function/GaussianBlur.py
@@ -1,27 +1,39 @@
 import cv2
-import matplotlib.pyplot as plt
 from PIL import Image
-import os
-import Image_information
+import numpy
+from function import Image_information
 
-if __name__ == '__main__':
-    # 读取图像
-    img = cv2.imread('../image/image.jpg')
 
-    # 高斯滤波
-    blur = cv2.GaussianBlur(img, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)
-    cv2.imwrite('../image/blur.jpg', blur)
-    blur1 = Image.open('../image/blur.jpg')
-    blur2 = blur1.convert('L')
-    blur2.save('../image/GaussianBlur.jpg')
-    print('Original image size: ', Image_information.GetFileSize('../image/image.jpg'))
-    print('GaussianBlur image size: ', Image_information.GetFileSize('../image/GaussianBlur.jpg'))
-    # print('Original image size: ', os.path.getsize('../image/image.jpg'))
-    # print('GaussianBlur image size: ', os.path.getsize('../image/GaussianBlur.jpg'))
+def gaussian(image):
+    # image = cv2.imread(path)  # 读取图像
+    blur = cv2.GaussianBlur(image, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)  # 高斯滤波
+    blur = Image.fromarray(numpy.uint8(blur))  # 将数组转为图像
+    blur = blur.convert('L')  # 将图像位深度转为8位
+    blur.save('image/GaussianBlur.jpg')  # 保存图像
+    print('Original image size: ', Image_information.GetFileSize('image/image.jpg'))
+    print('GaussianBlur image size: ', Image_information.GetFileSize('image/GaussianBlur.jpg'))  # 获取文件大小
 
-    # 显示结果
-    plt.subplot(121), plt.imshow(img), plt.title('Original')
-    plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(blur), plt.title('GaussianBlur')
-    plt.xticks([]), plt.yticks([])
-    plt.show()
+    return blur
+
+# if __name__ == '__main__':
+#     image = cv2.imread('../image/image.jpg')  # 读取图像
+#     blur = cv2.GaussianBlur(image, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)  # 高斯滤波
+#     blur = Image.fromarray(numpy.uint8(blur))  # 将数组转为图像
+#     blur = blur.convert('L')  # 将图像位深度转为8位
+#     blur.save('../image/GaussianBlur.jpg')  # 保存图像
+#     print('Original image size: ', Image_information.GetFileSize('../image/image.jpg'))
+#     print('GaussianBlur image size: ', Image_information.GetFileSize('../image/GaussianBlur.jpg'))  # 获取文件大小
+#
+
+    #
+    # # 读取图像
+    # img = cv2.imread('../image/image.jpg')
+    #
+    # # 高斯滤波
+    # blur = cv2.GaussianBlur(img, (5, 5), 0, borderType=cv2.BORDER_REPLICATE)
+    # cv2.imwrite('../image/blur.jpg', blur)
+    # blur1 = Image.open('../image/blur.jpg')
+    # blur2 = blur1.convert('L')
+    # blur2.save('../image/GaussianBlur.jpg')
+    # print('Original image size: ', Image_information.GetFileSize('../image/image.jpg'))
+    # print('GaussianBlur image size: ', Image_information.GetFileSize('../image/GaussianBlur.jpg'))
