efe72fb3d5952dd4b8ddcf11bcc5a73d0b1aedd5
Accdia <accidia12139@outlook.com>
2023-03-13 17:49:49 +0800

    修改内容

--- Python/main.py
+++ Python/main.py
@@ -1,20 +1,13 @@
 import cv2
+import numpy
 
 # # 动态的将搜索路径添加到sys.path中
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from function import GaussianBlur, Image_information
+from function import GaussianBlur, Image_information, rrcfileters
 
 if __name__ == '__main__':
     image = cv2.imread('image/image.jpg')
     print('Original image size= ', Image_information.getfilesize('image/image.jpg'))
     for size in range(3, 17, 2):
         GaussianBlur.gaussian(size, image)
-    # image = GaussianBlur.gaussian(5, cv2.imread('image/image.jpg'))
-
-    # print('Original image size: ', Image_information.GetFileSize('image/image.jpg'))
-    # print('GaussianBlur image size: ', Image_information.GetFileSize('image/GaussianBlur.jpg'))  # 获取文件大小
-
-    # image_colr_depth = Image_information.image_color_depth(Image.open('image/image.jpg'))
-    # image_size = Image_information.image_size(Image.open('image/image.jpg'))
-    # print(image_colr_depth, image_size)
